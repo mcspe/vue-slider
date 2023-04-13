d84cf37be475d75fc4e51ed741584be6bf0f23ce
mcspe <mcspezzaferro@gmail.com>
2023-04-13 15:33:34 +0200

    [add] click on thumb

--- js/script.js
+++ js/script.js
@@ -8,25 +8,32 @@ createApp({
       imgDB : [
         {
           src: './img/01.webp',
-          alt: 'img'
+          alt: 'img',
+          isActive: true
         },
         {
           src: './img/02.webp',
-          alt: 'img'
+          alt: 'img',
+          isActive: false
         },
         {
           src: './img/03.webp',
-          alt: 'img'
+          alt: 'img',
+          isActive: false
         },
         {
           src: './img/04.webp',
-          alt: 'img'
+          alt: 'img',
+          isActive: false
         },
         {
           src: './img/05.webp',
-          alt: 'img'
+          alt: 'img',
+          isActive: false
         }
-      ]
+      ],
+      active: 'active',
+      isPlaying: false
     }
   },
 
@@ -37,10 +44,12 @@ createApp({
       if(this.imgCounter > 0){
 
         this.imgCounter--;
+        this.getClassActive(this.imgCounter);
 
       }else{
 
         this.imgCounter = this.imgDB.length - 1;
+        this.getClassActive(this.imgCounter);
 
       }
     },
@@ -50,18 +59,37 @@ createApp({
       if(this.imgCounter < this.imgDB.length - 1){
 
         this.imgCounter++;
+        this.getClassActive(this.imgCounter);
 
       }else{
 
         this.imgCounter = 0;
+        this.getClassActive(this.imgCounter);
 
       }
+    },
+
+    imgActivate(index){
+      this.imgCounter = index;
+      this.getClassActive(index);
+    },
+
+    getClassActive(index){
+      this.imgDB.forEach(image => {
+        image.isActive = false;
+      });
+      this.imgDB[index].isActive = true;
+      //console.log(this.imgDB[index].isActive);
+    },
+
+    play(){
+      console.log('ciao');
     }
 
   },
 
   mounted(){
-    console.log(this.imgCounter);
+    //console.log(this.imgCounter);
   }
 
 }).mount('#app');
